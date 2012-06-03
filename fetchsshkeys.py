743a7fbe238a8871bd8f997db056bd1491c5b0a3
Matjaz Pecan <matjaz.pecan@gmail.com>
2012-06-03 12:21:14 +0200

    Code refactoring, cleanup.

--- fetchsshkeys.py
+++ fetchsshkeys.py
@@ -1,6 +1,6 @@
 #!/usr/local/bin/env python
-#
-# -*-python-*-
+# -*- coding:utf-8 -*-
+# -*- python -*-
 #
 ################################################################################
 # License
@@ -21,17 +21,25 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 ################################################################################ 
 #
-# Based on the updatesshkeys.py script by Cándido Rodríguez Montes found at
+# Based on the updatesshkeys.py script by Cándido Rodríguez Montes <kan@cica.es> found at
 # https://forja.rediris.es/projects/fedssh/ 
 # Converted for use with gitolite to retrieve keys by Matjaž Pečan <matjaz.pecan@gmail.com>
 
 
-import ldap, os, sys, time
+import  os, sys, time, errno, subprocess
 
+try:
+  import ldap
+except ImportError:
+  print("Unable to locate the 'ldap' module.  Please install python-ldap.  " \
+        "(http://python-ldap.sourceforge.net)")
+  sys.exit(1)
+  
 # Configuration options
-server = 'ldaps://ldap.klika.si:636'
+# Server address
+server = 'ldap://ldap.iaeste.net:389'
+# Base DN to search under
 dnbase = 'ou=people,dc=iaeste,dc=net'
-#dnuser = 'uid=git,ou=people,dc=iaeste,dc=net'
 # Bind user and password
 dnuser = ''
 dnpass = ''
@@ -39,62 +47,61 @@ dnpass = ''
 objectclass = 'account'
 # User name attribute
 usratt  = 'uid'
+# Group attribute
+groupatt = 'host'
 # SSH key attribute
 keyatt = 'sshPublicKey'
-host   = 'git'
+# Group griterion
+group_criteria   = 'git'
 keys_folder = '/home/git/.gitolite/keys'
-users = []
-l = ldap.initialize(server)
-try:
-	l.simple_bind_s(dnuser, dnpass)
-	filter = '(&(objectClass='+objectclass+')(host='++'))'
-	result_id =  l.search(dnbase, ldap.SCOPE_ONELEVEL, filter, [usratt])
-	while 1:
-		result_type, result_data = l.result(result_id, 0)
-		if result_data == []:
-			break;
-		elif result_type == ldap.RES_SEARCH_ENTRY:
-			for i in range(len(result_data)):
-				users.append(result_data[i][1][usratt][0])
-	for data_users in users:
-		keys = []
-		filter = '(&(objectClass='+objectclass+')('+useratt+'=' + data_users + '))'
-		result_id = l.search(dnbase, ldap.SCOPE_ONELEVEL, filter, [keyatt])
-		try:
-			os.makedirs(keys_folder)
-		except OSError as e:
-			if e.errno != errno.EEXIST:
-				raise
-		os.chgdir(keys_folder)
+
+def main():
+	l = ldap.initialize(server)
+	try:
+		l.simple_bind_s(dnuser, dnpass)
+		filter = '(&(objectClass='+objectclass+')('+groupatt+'='+group_criteria+'))'
+		result_id = l.search(dnbase, ldap.SCOPE_ONELEVEL, filter)
+		# Make sure our directory exists
+		create_directory(keys_folder)
+		# Change current path to keys directory
+		os.chdir(keys_folder)
 		while 1:
 			result_type, result_data = l.result(result_id, 0)
 			if result_data == []:
 				break;
 			elif result_type == ldap.RES_SEARCH_ENTRY:
 				for i in range(len(result_data)):
-					for key in result_data[i][1][keyatt]:
-						keys.append(key)
-		for i in range(len(keys)):
-			try:
-				os.mkdir(str(i)+'/')
-			except OSError as e:
-				if e.errno != errno.EEXIST:
-					raise
-			fd = open(str(i)+'/'+data_user+'.pub', "w")
-			fd.write(key[i])
-			fd.close()
-	os.exec('gitolite trigger SSH_AUTHKEYS')
+					# Get username
+					user = result_data[i][1][usratt]
+					for j in range(len(result_data[i][1][keyatt])):
+						# Create subdir in case it does not exist
+						create_directory(str(j)+'/')
+						fd = open(str(j)+'/'+user+'.pub', "w")
+						fd.write(result_data[i][1][keyatt][j])
+						fd.close()					
+		subprocess.call(['gitolite', 'trigger SSH_AUTHKEYS'])
+	
+	except ldap.INVALID_CREDENTIALS:
+		print "User or password is incorrect."
+		sys.exit()
+	
+	except ldap.LDAPError, e:
+		if type(e.message) == dict and e.message.has_key('desc'):
+			print e.message['desc']
+		else:
+			print e
+		sys.exit()
+	l.unbind()
+	
+#main()
 
-except ldap.INVALID_CREDENTIALS:
-	print "User or password is incorrect."
-	sys.exit()
+def create_directory(path):
+	try:
+		os.makedirs(path)
+	except OSError as e:
+		if e.errno != errno.EEXIST:
+			raise
 
-except ldap.LDAPError, e:
-	if type(e.message) == dict and e.message.has_key('desc'):
-		print e.message['desc']
-	else:
-		print e
-	sys.exit()
 
-l.unbind()
-
+if __name__ == "__main__":
+  main()
